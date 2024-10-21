ab83df789a894c70b5003de5426b44e552cd0ccf
Eduardo <130381385+eduardosantosp@users.noreply.github.com>
2024-10-21 15:16:57 +0000

    estruturas de pastas + middlewer

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-var http = require("http");
+const express = require("express");
+const app = express();
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello from CodeSandbox!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const PORT = 3343;
+//CRIAÇÃO DE ROTAS
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+//CRIAÇÃO ROTA 404 (MIDDLEWARE)
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
+});
+
+app.listen(PORT, () => {
+  console.log("running..." + PORT);
+});
